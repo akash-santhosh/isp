08bd474af9519e17996f751c722863f69359dc6e
Akash Santhosh <as23aad@herts.ac.uk>
2024-03-31 06:26:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 
 function App() {
   //creating IP state
-  const [query, setIP] = useState("");
-  const [country, setCountry] = useState("");
-  const [regionName, setRegion] = useState("");
-  const [timezone, setTimezone] = useState("");
-  const [isp, setIsp] = useState("");
-  const [as, setAs] = useState("");
-  const mobile = useState(false);
-  const proxy = useState(false);
-  const hosting = useState(false);
 
+  const [ipData, setIpData] = useState({});
 
   const getData = async () => {
-    const res = await axios.get("http://ip-api.com/json/");
+    const res = await axios.get("http://ip-api.com/json/?fields=16984841");
     console.log(res.data);
-    setCountry(res.data.country);
-    setRegion(res.data.regionName);
-    setTimezone(res.data.timezone);
-    setIsp(res.data.isp);
-    setAs(res.data.as);
+    setIpData({
+      ip: res.data.query,
+      country: res.data.country,
+      region: res.data.regionName,
+      timezone: res.data.timezone,
+      isp: res.data.isp,
+      as: res.data.as,
+      mobile: res.data.mobile,
+      proxy: res.data.proxy,
+      hosting: res.data.hosting,
+    });
   };
 
   useEffect(() => {
@@ -36,15 +28,19 @@ function App() {
     getData();
   }, []);
 
+  if (!ipData) return <h1>Loading...</h1>;
+
   return (
     <div className="App">
-      <h1>Network Details</h1>
-        <h2>Country : {country}</h2>
-        <h2>Region : {regionName}</h2>
-        <h2> Time Zone : {timezone}</h2>
-        <h2> ISP Name : {isp}</h2>
-        <h2> AS : {as}</h2>
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Here are your Public Network Details</h1>
+      <h2>Country : {ipData.country}</h2>
+      <h2>Region : {ipData.region}</h2>
+      <h2> Time Zone : {ipData.timezone}</h2>
+      <h2> ISP Name : {ipData.isp}</h2>
+      <h2> AS : {ipData.as}</h2>
+      <h2> Is a Mobile Network ? : {String(ipData.mobile)}</h2>
+      <h2> Is a Proxy Network ? : {String(ipData.proxy)}</h2>
+      <h2> Is a Hosted Network ? : {String(ipData.hosting)}</h2>
     </div>
   );
 }
