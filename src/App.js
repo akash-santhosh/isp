bdef8cc845a3d2106eb6779860fb5db48a9ad8a9
Akash Santhosh <as23aad@herts.ac.uk>
2024-03-31 05:22:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function App() {
+  //creating IP state
+  const [query, setIP] = useState("");
+  const [country, setCountry] = useState("");
+  const [regionName, setRegion] = useState("");
+  const [timezone, setTimezone] = useState("");
+  const [isp, setIsp] = useState("");
+  const [as, setAs] = useState("");
+  const mobile = useState(false);
+  const proxy = useState(false);
+  const hosting = useState(false);
+
+
+  const getData = async () => {
+    const res = await axios.get("http://ip-api.com/json/");
+    console.log(res.data);
+    setCountry(res.data.country);
+    setRegion(res.data.regionName);
+    setTimezone(res.data.timezone);
+    setIsp(res.data.isp);
+    setAs(res.data.as);
+  };
+
+  useEffect(() => {
+    //passing getData method to the lifecycle method
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>Network Details</h1>
+        <h2>Country : {country}</h2>
+        <h2>Region : {regionName}</h2>
+        <h2> Time Zone : {timezone}</h2>
+        <h2> ISP Name : {isp}</h2>
+        <h2> AS : {as}</h2>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
